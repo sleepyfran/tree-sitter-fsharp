425e43885c12204e9f338b9646479da052f201e2
sleepyfran <fgzv@outlook.com>
2022-12-30 18:15:43 +0100

    fix typar atomic_pat precedence
    TODO: Revisit. Again, no mention of precedence for these, so a conflict
    is needed.

--- grammar.js
+++ grammar.js
@@ -53,6 +53,7 @@ module.exports = grammar({
       [$.expr],
       [$.pat],
       [$.record_pat],
+      [$.typar, $.atomic_pat]
     ],
     /**
      * an array of token names which can be returned by an external scanner. External scanners allow you to write custom C code which runs during the lexing process in order to handle lexical rules (e.g. Python’s indentation tokens) that cannot be described by regular expressions.
